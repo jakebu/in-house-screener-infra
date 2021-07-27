ca84839cfa2be265b37fc4d4c946ba0d8a8b9000
Jake <jake.budin@gmail.com>
2021-07-26 23:34:43 -0400

    Provide reasoning for each resource

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
+// Initial wide range VPC
 resource "aws_vpc" "myvpc" {
   cidr_block = "172.16.0.0/16"
 
@@ -10,6 +11,7 @@ resource "aws_vpc" "myvpc" {
   }
 }
 
+// Initial private subnet
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.myvpc.id
   cidr_block        = "172.16.10.0/24"
@@ -20,6 +22,7 @@ resource "aws_subnet" "my_subnet" {
   }
 }
 
+// SG to allow TLS within VPC
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
@@ -46,6 +49,7 @@ resource "aws_security_group" "allow_tls" {
   }
 }
 
+// SG to allow RDP within VPC
 resource "aws_security_group" "allow_rdp" {
   name        = "allow_rdp"
   description = "Allow RDP inbound traffic"
@@ -72,6 +76,7 @@ resource "aws_security_group" "allow_rdp" {
   }
 }
 
+// Create NIC card with specific IP within the private subnet with both SG's attached
 resource "aws_network_interface" "my_nic" {
   subnet_id       = aws_subnet.my_subnet.id
   private_ips     = ["172.16.10.100"]
@@ -82,10 +87,12 @@ resource "aws_network_interface" "my_nic" {
   }
 }
 
+// Create EC2 using free Windows micro tier 
 resource "aws_instance" "myec2" {
   ami           = "ami-03295ec1641924349"
   instance_type = "t2.micro"
 
+// What does unlimited do?
   credit_specification {
     cpu_credits = "unlimited"
   }
@@ -95,6 +102,7 @@ resource "aws_instance" "myec2" {
   }
 }
 
+// Create SSM doc which runs a powershell script "ipconfig"
 resource "aws_ssm_document" "my_doc" {
   name          = "my_document"
   document_type = "Command"
@@ -116,6 +124,7 @@ resource "aws_ssm_document" "my_doc" {
 DOC
 }
 
+// Create a maintenance window for once a week on Sundays, 2 hour duration, 1 hour cutoff
 resource "aws_ssm_maintenance_window" "weekend" {
   name     = "maintenance-window-application"
   schedule = "cron(0 15 ? * SUN *)"
@@ -123,6 +132,7 @@ resource "aws_ssm_maintenance_window" "weekend" {
   cutoff   = 1
 }
 
+// Create an S3 bucket to hold results of ssm maintenance window
 resource "aws_s3_bucket" "my-bucket-screener-jb-test" {
   bucket = "my-bucket-screener-jb-test"
 
@@ -131,6 +141,7 @@ resource "aws_s3_bucket" "my-bucket-screener-jb-test" {
   }
 }
 
+// Create SSM task which runs during the SSM window, targeting myec2 and sending output to my-bucket-screener-jb-test S3 bucket
 resource "aws_ssm_maintenance_window_task" "my_task" {
   max_concurrency = 2
   max_errors      = 1
@@ -152,6 +163,8 @@ resource "aws_ssm_maintenance_window_task" "my_task" {
     }
   }
 }
+
+// Specify the SSM window target instance by tag Name primary_instance
 resource "aws_ssm_maintenance_window_target" "my_target" {
   window_id     = aws_ssm_maintenance_window.weekend.id
   name          = "maintenance-window-target"
@@ -164,6 +177,7 @@ resource "aws_ssm_maintenance_window_target" "my_target" {
   }
 }
 
+// Associate my_doc SSM doc with myec2
 resource "aws_ssm_association" "my_ssm_association" {
   name = aws_ssm_document.my_doc.name
 
